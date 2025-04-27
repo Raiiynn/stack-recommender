05a7bb056a9b769e420c96e28a9496b1455d0657
Raiiynn <email.githubdaffa@gmail.com>
2025-04-27 11:28:12 +0700

    fix: update tailwind config and postcss setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
 
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export default config;
